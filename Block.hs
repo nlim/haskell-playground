a8e71555716261aad90ca121b01c6a0b71c17c5b
Nathaniel Lim <nathaniel.j.lim@gmail.com>
2013-09-29 00:32:03 -0400

    hlint

--- Block.hs
+++ Block.hs
@@ -2,6 +2,7 @@ import qualified Data.IntMap as M
 import System.Environment
 import Control.Parallel.Strategies (Strategy, runEval, parMap, parBuffer, rpar, evalTuple2, rseq, rdeepseq, using, withStrategy)
 import Data.Set (Set)
+import Data.List (foldl')
 import qualified Data.Set as Set
 
 
@@ -28,23 +29,23 @@ getPermutations :: RowCombo -> [[Block]]
 getPermutations rc = go rc [[]]
                      where go :: RowCombo -> [[Block]] -> [[Block]]
                            go (RowCombo 0 0) results = results
-                           go (RowCombo i 0) results = [r ++ (take i (repeat ThreeBlock)) | r <- results]
-                           go (RowCombo 0 j) results = [r ++ (take j (repeat FourFiveBlock)) | r <- results]
-                           go (RowCombo i j) results = (go (RowCombo (i-1) j) ([r ++ [ThreeBlock] | r <- results])) ++ (go (RowCombo i (j-1)) ([r ++ [FourFiveBlock] | r <- results]))
+                           go (RowCombo i 0) results = [r ++ replicate i ThreeBlock | r <- results]
+                           go (RowCombo 0 j) results = [r ++ replicate j FourFiveBlock | r <- results]
+                           go (RowCombo i j) results = go (RowCombo (i-1) j) [r ++ [ThreeBlock] | r <- results] ++ go (RowCombo i (j-1)) [r ++ [FourFiveBlock] | r <- results]
 
 --- Get all the combinations of
 --- choosing from the two types of blocks that add up to w
 getCombos :: Int -> [RowCombo]
 getCombos w = [ RowCombo i j | i <- [0..max3], j <- [0..max45], ((i*6) + (j*9)) == (2*w) ]
-              where max3  = ((2*w) `div` 6)
-                    max45 = ((2*w) `div` 9)
+              where max3  = (2 * w) `div` 6
+                    max45 = (2 * w) `div` 9
 
 
 --- Get a List of all the x coordinates (times 2) where there is an intersection
 --- of two blocks, that is not at either end
 dJunctures :: [Block] -> [Int]
-dJunctures bs = filter (\i -> i /= 0 && i /= w) ((fst r):(snd r))
-                where r = (foldl (\(t, ts) b -> (t + (doubleBlockWidth b), t:ts)) (0,[]) bs)
+dJunctures bs = filter (\i -> i /= 0 && i /= w) (uncurry (:) r)
+                where r = foldl' (\(t, ts) b -> (t + doubleBlockWidth b, t:ts)) (0,[]) bs
                       w = sum (map doubleBlockWidth bs)
 
 --- We can build a Map from blockId => Set of blockIds that are valid to be adjacent to blockId
@@ -52,14 +53,14 @@ dJunctures bs = filter (\i -> i /= 0 && i /= w) ((fst r):(snd r))
 getValidAdjMap :: M.IntMap [Block] -> M.IntMap (Set Int)
 getValidAdjMap blockMap = M.fromList (withStrategy (parBuffer 100 rdeepseq) (map (\i -> (i, valids i)) (M.keys bIdToDj)))
                        where
-                          valids :: Int -> (Set Int)
-                          valids bid = foldl
-                            (\s dj -> remBlockIdsForDj dj s)
+                          valids :: Int -> Set Int
+                          valids bid = foldl'
+                            (flip remBlockIdsForDj)
                             (Set.fromList (M.keys bIdToDj))
                             (bIdToDj M.! bid)
 
-                          remBlockIdsForDj :: Int -> (Set Int) -> (Set Int)
-                          remBlockIdsForDj dj s = foldl (\s' bid ->  Set.delete bid s') s (dJunctureToBlockIds M.! dj)
+                          remBlockIdsForDj :: Int -> Set Int -> Set Int
+                          remBlockIdsForDj dj s = foldl' (flip Set.delete) s (dJunctureToBlockIds M.! dj)
                           dJunctureToBlockIds = groupKeysByValue bIdToDj
                           bIdToDj = bIdToDj' blockMap
 
@@ -68,7 +69,7 @@ getValidAdjMap blockMap = M.fromList (withStrategy (parBuffer 100 rdeepseq) (map
 groupKeysByValue :: M.IntMap [Int] -> M.IntMap [Int]
 groupKeysByValue x = foldl
   (\m i -> foldl
-    (\m' j -> case (M.lookup j m') of
+    (\m' j -> case M.lookup j m' of
                 Just is -> M.insert j (i:is) m'
                 Nothing -> M.insert j [i] m')
     m
@@ -81,24 +82,23 @@ groupKeysByValue x = foldl
 --- Given a IntMap which just the Block configurations labeled by an integer Key
 --- Produce an IntMap which provides all the Block juncture points for a given integer key label for
 --- the block configuration
-bIdToDj' :: (M.IntMap [Block]) -> M.IntMap [Int]
+bIdToDj' :: M.IntMap [Block] -> M.IntMap [Int]
 bIdToDj' blockMap = M.fromList $ map (\i -> (i, dJunctures (blockMap M.! i))) (M.keys blockMap)
 
 
 
 allPermutations :: Int -> [[Block]]
-allPermutations width = do c <- getCombos(width)
-                           p <- getPermutations(c)
-                           return p
+allPermutations width = do c <- getCombos width
+                           getPermutations c
 
-getLabelMap :: [a] -> (M.IntMap a)
+getLabelMap :: [a] -> M.IntMap a
 getLabelMap as = M.fromList $ zip [1..] as
 
 numWays :: Int -> Int -> Int
 numWays width height = (sum . M.elems) wayMap
                        where
                          wayMap = wayMapH 1 (M.fromList [ (i,1) | i <- labels ])
-                         wayMapH :: Int -> (M.IntMap Int) -> (M.IntMap Int)
+                         wayMapH :: Int -> M.IntMap Int -> M.IntMap Int
                          wayMapH n m
                                 | n <= 0 = M.fromList [ (i,0) | i <- labels]
                                 | n >= height = m
@@ -109,14 +109,14 @@ numWays width height = (sum . M.elems) wayMap
                          labels = M.keys labelMap
                          labelMap = getLabelMap (allPermutations width)
 
-parseInt :: [Char] -> Int
-parseInt s = read s
+parseInt :: String -> Int
+parseInt = read
 
 -- Run this
 main :: IO ()
 main = do
   [ws,hs] <- getArgs
-  putStrLn $ show $ numWays (parseInt ws) (parseInt hs)
+  print (numWays (parseInt ws) (parseInt hs))
 
 --- time ./Block 48 10  +RTS -N4 -l
 --- 806844323190414
