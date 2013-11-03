86222a178d3e666f7253d6f64dde0c9accee11b3
nlim <nathaniel.j.lim@gmail.com>
2013-11-03 16:46:43 -0500

    foo

--- HigherRank.hs
+++ HigherRank.hs
@@ -1,5 +1,20 @@
+import Data.STRef
+import Control.Monad.ST.Safe
+import Control.Monad (replicateM_)
+
+main = (putStrLn . show) stResult
+
+stResult = runST $ do ref <- newSTRef 0
+                      replicateM_ 1000000 $ modifySTRef' ref (+1)
+                      readSTRef ref
+
+--- Can't be done!
+--- v  = runST (newSTRef "abc")
+
 foo :: (forall a. a -> a) -> (Char,Bool)
 foo f = (f 'c', f True)
 
 bar :: forall a. ((a -> a) -> (Char, Bool))
 bar f = ('c', True)
+
+
