7c4ea6ddf7ae8726e93bcb6d06f66ec36219a665
nlim <nathaniel.j.lim@gmail.com>
2013-11-09 20:10:09 -0500

    concur

--- Concur.hs
+++ Concur.hs
@@ -1,12 +1,39 @@
+{-# LANGUAGE BangPatterns #-}
 import Control.Concurrent
 import Control.Monad
 import System.IO
 import Control.Concurrent.STM.TVar
 import Control.Concurrent.STM
 
-main :: IO ()
+import Data.Digest.Pure.MD5
+import qualified Data.ByteString.Lazy as L
+import System.Environment
+import Control.Concurrent
+import Control.Concurrent.Chan
+import Control.Monad (forever, forM_, replicateM_)
 
-main = funTest
+nrWorkers = 2
+
+main :: IO ()
+main = channelProg
+
+channelProg = do files <- getArgs
+                 str <- newChan
+                 fileChan <- newChan
+                 forM_ [1..nrWorkers] (\_ -> forkIO $ worker str fileChan)
+                 forM_ files (writeChan fileChan)
+                 printNrResults (length files) str
+
+printNrResults i var = replicateM_ i (readChan var >>= putStrLn)
+
+worker :: Chan String -> Chan String -> IO ()
+worker str fileChan = forever (readChan fileChan >>= hashAndPrint str)
+
+hashAndPrint str f = do
+        bs <- L.readFile f
+        let !h = show $ md5 bs
+        writeChan str (f ++ ": " ++ h)
+
 
 funTest :: IO ()
 funTest = do
